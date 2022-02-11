0a0b47c5a2ef8f22e92ee91b6267ff4138bedb9f
Rosalioalfre2 <rosalioalfre2@gmail.com>
2022-02-11 15:33:46 -0600

    Inicializando

---
+++ includes/funciones.php
@@ -0,0 +1,50 @@
+<?php
+
+function obtenerServicios() : array {
+    try {
+        //Importar una conexion
+        require 'database.php';
+
+        //Escribir el codigo SQL
+        $sql = "SELECT * FROM `servicios`;";
+
+        $consulta = mysqli_query($db, $sql);
+
+        //Obtener los resultados
+        echo "<pre>";
+        
+        //Mostrar los resultados
+        /*
+        *Version 01
+        //var_dump( mysqli_fetch_all($consulta) );
+        var_dump( mysqli_fetch_assoc($consulta) );
+        echo "</pre>";
+        */
+
+        /*Version 02*/
+
+        //Areglo vacio
+        $servicios = [];
+
+        $i = 0; //*Contador
+
+        while ( $row = mysqli_fetch_assoc( $consulta ) ){
+            $servicios[$i]['id'] = $row ['id'];
+            $servicios[$i]['nombre'] = $row ['nombre'];
+            $servicios[$i]['precio'] = $row ['precio'];
+
+            $i++;
+        }
+
+	    /*echo "<pre>";
+        var_dump( $servicios );
+        echo "</pre>"; */
+
+        return $servicios;
+
+    } catch (\Throwable $th) {
+        var_dump($th);
+    }
+}
+
+var_dump( obtenerServicios() );
